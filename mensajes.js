4010ae4793bb1768cfa463ab46b5ba99a939f4c2
Sarellano CASTILLO <105746337+Ligthning24@users.noreply.github.com>
2025-08-02 18:42:30 -0600

    message

--- mensajes.js
+++ mensajes.js
@@ -7,9 +7,8 @@ export default async function handleMessages(req, res) {
   const msg = req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
   if (msg?.text?.body) {
     console.log('▶️ Incoming:', msg.text.body);
-    // Responde con un echo
+    // Responde con el mismo texto
     await sendText(msg.from, `Echo: ${msg.text.body}`);
   }
-  // Siempre responde 200 OK a Meta
   return res.sendStatus(200);
 }
